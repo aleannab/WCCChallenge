fb95c752837e9e197cf1e5b069984b04696b6589
aleannab <aleannab@gmail.com>
2023-11-29 10:22:30 -0800

    [obscure]  Update row/col count based on padding

--- 2023-11-26_obscure/sketch.js
+++ 2023-11-26_obscure/sketch.js
@@ -1,8 +1,9 @@
 let gWorld;
-let gRowCount = 30;
-let gColCount = 30;
+let gRowPadding = 20;
+let gColPadding = 20;
+let gRowCount;
+let gColCount;
 let gParticles = [];
-
 
 function setup() {
   createCanvas(0.9 * windowWidth, 1.0 * windowHeight);
@@ -11,21 +12,21 @@ function setup() {
 
   gWorld = new c2.World(new c2.Rect(0, 0, width, height));
   createWeb();
-  
+
   stroke(0);
   strokeWeight(25);
-  fill(0);
-
 }
 
 function draw() {
-  background("#FFFFFF");
+  background('#FFFFFF');
+  fill('#FF0000');
+  noStroke();
+  circle(width - mouseX, height - mouseY, 200);
 
   let hasMoved = abs(movedX) > 1 && abs(movedY) > 1;
 
-
   if (hasMoved) {
-     let mouseForce = new c2.PointField(new c2.Point(mouseX, mouseY), -30);
+    let mouseForce = new c2.PointField(new c2.Point(mouseX, mouseY), -30);
 
     gParticles.forEach((ref) => {
       let staticStrength = 5;
@@ -34,34 +35,34 @@ function draw() {
       } else {
         mouseForce.apply(ref.particle);
       }
-
-
     });
   }
- gParticles.forEach((ref) => {
+  gParticles.forEach((ref) => {
     let staticStrength = 5;
     if (ref.isStatic) {
       staticStrength = 20;
     }
-let staticForce = new c2.PointField(new c2.Point(ref.initX, ref.initY), staticStrength);
-  staticForce.apply(ref.particle);
+    let staticForce = new c2.PointField(new c2.Point(ref.initX, ref.initY), staticStrength);
+    staticForce.apply(ref.particle);
   });
-
-  
-
 
   gWorld.update();
 
+  noFill();
+  stroke(0);
   for (let i = 0; i < gColCount; i++) {
-    noFill();
     beginShape();
+    let firstParticle = gParticles[getPIndex(i, 0)];
+
+    curveVertex(firstParticle.particle.position.x, -100);
     for (let j = 0; j < gRowCount; j++) {
       let p = gParticles[getPIndex(i, j)];
       curveVertex(p.particle.position.x, p.particle.position.y);
+      //circle(p.particle.position.x, p.particle.position.y, 10);
     }
+    curveVertex(firstParticle.particle.position.x, height + 100);
     endShape();
   }
-  
 }
 
 function createParticle(posX, posY) {
@@ -79,18 +80,22 @@ function getPIndex(rowIndex, colIndex) {
 
 function createWeb(parent, level) {
   color(0);
-  let buffer = 100;
-  let rowPad = floor((width + 2* buffer) / (gRowCount - 1));
-  let columnPad = floor((height + 2* buffer) / (gColCount - 1));
+  gRowCount = floor(height / gRowPadding) + 1;
+
+  let adjRowPad = floor(height / (gRowCount - 1));
+
+  gColCount = floor(width / gColPadding) + 1;
+  let adjColPad = floor(width / (gColCount - 1));
+
   let lastRow = gRowCount - 1;
   let lastCol = gColCount - 1;
   for (let i = 0; i < gColCount; i++) {
     for (let j = 0; j < gRowCount; j++) {
-      let x = i * rowPad - buffer;
-      let y = j * columnPad - buffer;
-      let p = createParticle(i * rowPad - buffer, j * columnPad - buffer);
-      let static = i === 0 || i === lastCol || j === 0 || j === lastRow ? true : false;
-      let particleRef = { particle: p, isStatic: static, initX: x, initY: y};
+      let x = i * adjColPad;
+      let y = j * adjRowPad;
+      let p = createParticle(x, y);
+      let static = j === 0 || j === lastRow ? true : false;
+      let particleRef = { particle: p, isStatic: static, initX: x, initY: y };
       gParticles.push(particleRef);
     }
   }
