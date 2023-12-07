3d7faff45a7b04e6679b2a799531897590dd97a3
aleannab <aleannab@gmail.com>
2023-12-07 00:31:18 -0800

    [slugs] Add style to slugs

--- 2023-12-03_slugs/sketch.js
+++ 2023-12-03_slugs/sketch.js
@@ -12,9 +12,14 @@ let gIsDebug = false;
 let gBuffer = 50;
 let gResetTime = 0;
 
+let gBgColor = '#09090b';
+let gSlugPalette = ['#79b7ba', '#9f1fde', '#fb6a0c', '#8de28e', '#e3dee6'];
+let gCircleSizes = [9, 12, 15];
+
 function setup() {
   // init sizes
-  createCanvas(windowWidth, windowHeight);
+  let scalar = 0.9;
+  createCanvas(scalar * windowWidth, scalar * windowHeight);
   gWorld = new c2.World(new c2.Rect(0, -gBuffer, width, height + gBuffer));
 
   if (gIsDebug) {
@@ -33,12 +38,12 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+  background(gBgColor);
 
   //if (!gIsDebug) {
   let time = millis() - gResetTime;
-  if (time > 1000 && gSlugs.length < 50) {
-    createSlug(random(500, width), -gBuffer / 2);
+  if (time > 1000 && gSlugs.length < 100) {
+    createSlug(random(0, width), -gBuffer / 2);
     gResetTime = millis();
   }
   gWorld.update();
@@ -65,12 +70,14 @@ class Slug {
     this.width = random(15, 25);
     this.halfWidth = this.width / 2;
     this.segmentCount = floor(random(5, 10));
-    this.segmentLength = this.width; //d random(25, 40);
+    this.segmentLength = random(0.8, 1.2) * this.width;
     this.points = [];
     this.springs = [];
-    this.circleSize = floor(random(5, 15));
+    this.circleSize = random(gCircleSizes);
+    this.mass = random(1, 5);
+    this.isDots = false; //random(0, 1) > 0.5;
 
-    this.color = color(random(0, 255), random(0, 255), random(0, 255));
+    this.color = random(gSlugPalette);
 
     this.createBody(this.segmentLength, this.segmentCount);
   }
@@ -81,26 +88,17 @@ class Slug {
     let posY = this.pos.y;
     let lengthScalar = 1;
 
-    let head;
-    let topPts = [];
-    let bottomPts = [];
-    let end;
-
     for (let i = 0; i < segCount; i++) {
       let currentSegment = [];
 
       if (i === 0) {
-        head = this.createParticle(posX, posY, true);
-        currentSegment.push(head);
+        currentSegment.push(this.createParticle(posX, posY, true));
       } else if (i === segCount - 1) {
-        end = this.createParticle(posX, posY + this.halfWidth, true);
-        currentSegment.push(end);
+        currentSegment.push(this.createParticle(posX, posY, false));
         this.createSpringsFromSegments(currentSegment, prevSegment, lengthScalar);
       } else {
         let top = this.createParticle(posX, posY - this.halfWidth);
-        topPts.push(top);
         let bottom = this.createParticle(posX, posY + this.halfWidth);
-        bottomPts.push(bottom);
         currentSegment.push.apply(currentSegment, [top, bottom]);
         this.createSpring(top, bottom, lengthScalar, 1);
         this.createSpringsFromSegments(currentSegment, prevSegment, lengthScalar);
@@ -113,7 +111,7 @@ class Slug {
   createParticle(posX, posY, isFixed = false) {
     let p = new c2.Particle(posX, posY);
     p.radius = 10;
-    p.mass = 1; //p.radius;
+    p.mass = random(0.9, 1.1) * this.mass; //random(1, 5); //1; //p.radius;
     if (isFixed) p.fix();
     gWorld.addParticle(p);
     this.points.push(p);
@@ -141,11 +139,26 @@ class Slug {
   }
 
   draw() {
+    //if (this.isDots) {
     noStroke();
     fill(this.color);
     for (let point of this.points) {
       circle(point.position.x, point.position.y, this.circleSize);
     }
+    //} else {
+    noFill();
+    stroke(this.color);
+    strokeWeight(1);
+    beginShape();
+    let first = this.points[0];
+    let last = this.points[this.points.length - 1];
+    curveVertex(first.position.x, first.position.y);
+    for (let point of this.points) {
+      curveVertex(point.position.x, point.position.y);
+    }
+    curveVertex(last.position.x, last.position.y);
+    endShape();
+    //}
 
     if (gIsDebug) {
       noFill();
