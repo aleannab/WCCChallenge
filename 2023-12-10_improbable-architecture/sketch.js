a3b26c20c8b9512ab80a0b9dd328fef2710cb5f8
aleannab <aleannab@gmail.com>
2023-12-12 19:20:07 -0800

    [improb-architect] Code refactor

--- 2023-12-10_improbable-architecture/sketch.js
+++ 2023-12-10_improbable-architecture/sketch.js
@@ -1,11 +1,11 @@
 let gLeftPt;
 let gRightPt;
 
-let gUpperSections = [];
-let gMidSections = [];
-let gLowerSections = [];
+let gAllBlocks = [];
 
 let gWallDistMin = 50;
+
+let gIsDebug = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,14 +16,18 @@ function setup() {
   gRightPt = new c2.Vector(0.9 * windowWidth, 0.7 * windowHeight);
 
   let yp = windowHeight * 0.1;
+  let upper = [];
+  let mid = [];
+  let lower = [];
   for (let i = 0; i < 20; i++) {
-    let section = new WallSection(yp);
-    if (section.level > 0) gUpperSections.push(section);
-    else if (section.level < 0) gLowerSections.push(section);
-    else gMidSections.push(section);
+    let block = new BuildingBlock(yp);
+    if (block.level > 0) upper.push(block);
+    else if (block.level < 0) lower.push(block);
+    else mid.push(block);
     yp += random(25, 100);
   }
-  console.log(gUpperSections.length + ' ' + gMidSections.length + ' ' + gLowerSections.length);
+
+  gAllBlocks = [...upper, ...lower, ...mid];
   noFill();
 }
 
@@ -32,77 +36,67 @@ function draw() {
   background(255);
 
   // perspective lines
-  stroke(200);
-  strokeWeight(1);
+  strokeWeight(1); // draw horizon line
   line(0, gLeftPt.y, width, gRightPt.y);
-  stroke(255, 0, 0);
-  circle(gLeftPt.x, gLeftPt.y, 10);
-  circle(gRightPt.x, gRightPt.y, 10);
-
-  for (let section of gUpperSections) {
-    section.draw();
+  if (gIsDebug) {
+    // draw perspective pts
+    stroke(255, 0, 0);
+    circle(gLeftPt.x, gLeftPt.y, 10);
+    circle(gRightPt.x, gRightPt.y, 10);
   }
 
-  for (let section of gLowerSections) {
-    section.draw();
-  }
-
-  for (let section of gMidSections) {
-    section.draw();
+  // draw building sections
+  for (let block of gAllBlocks) {
+    block.draw();
   }
 }
 
-class WallSection {
+class BuildingBlock {
   constructor(yp) {
+    this.boundLines = [];
+    // center corner
     let centerUpperPt = new c2.Vector(random(0.3, 0.7) * width, yp);
     let centerLowerPt = new c2.Vector(centerUpperPt.x, centerUpperPt.y - random(50, 300));
     this.centerLine = new Line(centerUpperPt, centerLowerPt);
 
     // left side
-    this.leftUpperBoundLine = new Line(centerUpperPt, gLeftPt);
-    this.leftLowerBoundLine = new Line(centerLowerPt, gLeftPt);
     let leftPosX = floor(random(gLeftPt.x + gWallDistMin, centerUpperPt.x - gWallDistMin));
-    this.leftCornerLine = this.getCornerLine(leftPosX, this.leftUpperBoundLine, this.leftLowerBoundLine);
+    this.leftCornerLine = this.getCornerLine(leftPosX, this.centerLine, gLeftPt);
 
     // right side
-    this.rightUpperBoundLine = new Line(centerUpperPt, gRightPt);
-    this.rightLowerBoundLine = new Line(centerLowerPt, gRightPt);
     let rightPosX = floor(random(centerUpperPt.x + gWallDistMin, gRightPt.x - gWallDistMin));
-    this.rightCornerLine = this.getCornerLine(rightPosX, this.rightUpperBoundLine, this.rightLowerBoundLine);
+    this.rightCornerLine = this.getCornerLine(rightPosX, this.centerLine, gRightPt);
 
     // ceiling
-    this.leftCeilingBoundLine = new Line(this.leftCornerLine.p1, gRightPt);
-    this.rightCeilingBoundLine = new Line(this.rightCornerLine.p1, gLeftPt);
-    this.ceilingPt = this.getIntersectionPt(this.leftCeilingBoundLine, this.rightCeilingBoundLine);
+    this.ceilingPt = this.getIntersectionPt(new Line(this.leftCornerLine.p1, gRightPt), new Line(this.rightCornerLine.p1, gLeftPt), true);
 
     //floor
-    this.leftFloorBoundLine = new Line(this.leftCornerLine.p0, gRightPt);
-    this.rightFloorBoundLine = new Line(this.rightCornerLine.p0, gLeftPt);
-    this.floorPt = this.getIntersectionPt(this.leftFloorBoundLine, this.rightFloorBoundLine);
+    this.floorPt = this.getIntersectionPt(new Line(this.leftCornerLine.p0, gRightPt), new Line(this.rightCornerLine.p0, gLeftPt), true);
 
-    let leftQuad = [this.leftCornerLine.p0, this.centerLine.p0, this.centerLine.p1, this.leftCornerLine.p1];
-    let rightQuad = [this.centerLine.p0, this.rightCornerLine.p0, this.rightCornerLine.p1, this.centerLine.p1];
-    let bottomQuad = [this.leftCornerLine.p0, this.centerLine.p0, this.rightCornerLine.p0, this.floorPt];
-    let topQuad = [this.leftCornerLine.p1, this.centerLine.p1, this.rightCornerLine.p1, this.ceilingPt];
+    let leftWall = new Wall(this.leftCornerLine.p0, this.centerLine.p0, this.centerLine.p1, this.leftCornerLine.p1);
+    let rightWall = new Wall(this.centerLine.p0, this.rightCornerLine.p0, this.rightCornerLine.p1, this.centerLine.p1);
+    let floorWall = new Wall(this.leftCornerLine.p0, this.centerLine.p0, this.rightCornerLine.p0, this.floorPt);
+    let ceilingWall = new Wall(this.leftCornerLine.p1, this.centerLine.p1, this.rightCornerLine.p1, this.ceilingPt);
 
     if (centerUpperPt.y > gLeftPt.y && centerLowerPt.y > gLeftPt.y) {
-      this.allQuads = [topQuad, leftQuad, rightQuad];
+      this.allWalls = [ceilingWall, leftWall, rightWall];
       this.level = 1;
     } else if (centerUpperPt.y < gLeftPt.y && centerLowerPt.y < gLeftPt.y) {
-      this.allQuads = [leftQuad, rightQuad, bottomQuad];
+      this.allWalls = [leftWall, rightWall, floorWall];
       this.level = -1;
     } else {
-      this.allQuads = [leftQuad, rightQuad];
+      this.allWalls = [leftWall, rightWall];
       this.level = 0;
     }
     this.color = color(random(0, 360), 50, 200);
   }
 
-  getCornerLine(ptX, upperBound, lowerBound) {
-    let upperIntersection = this.getIntersectionPtWithConstant(ptX, upperBound);
-    let lowerIntersection = this.getIntersectionPtWithConstant(ptX, lowerBound);
+  getCornerLine(ptX, line, perspectivePt) {
+    let boundLines = [new Line(line.p0, perspectivePt), new Line(line.p1, perspectivePt)];
+    this.boundLines.push(...boundLines);
 
-    return new Line(upperIntersection, lowerIntersection);
+    let intersections = boundLines.map((boundLine) => this.getIntersectionPtWithConstant(ptX, boundLine));
+    return new Line(...intersections);
   }
 
   getIntersectionPtWithConstant(constant, line) {
@@ -112,7 +106,8 @@ class WallSection {
     return new c2.Vector(x, y);
   }
 
-  getIntersectionPt(line0, line1) {
+  getIntersectionPt(line0, line1, shouldAddToBoundLines = false) {
+    if (shouldAddToBoundLines) this.boundLines.push(...[line0, line1]);
     let x = (line1.yIntercept - line0.yIntercept) / (line0.slope - line1.slope);
     let y = line0.slope * x + line0.yIntercept;
     return new c2.Vector(x, y);
@@ -122,36 +117,36 @@ class WallSection {
     stroke(255, 0, 0);
     strokeWeight(0.5);
 
-    //perspective bounds left and right
-    stroke(hue(this.color), saturation(this.color), brightness(this.color), 100);
-    this.leftUpperBoundLine.draw();
-    this.leftLowerBoundLine.draw();
-    this.rightUpperBoundLine.draw();
-    this.rightLowerBoundLine.draw();
-
-    // stroke(255, 0, 0);
-    this.leftFloorBoundLine.draw();
-    this.rightFloorBoundLine.draw();
-
-    stroke(255);
-    this.leftCornerLine.draw();
-    this.rightCornerLine.draw();
-
-    // top and bottom points of center corner.
-    stroke(0);
-    // this.centerLine.draw();
+    if (true) this.drawBoundLines();
 
     fill(this.color);
-    for (let side of this.allQuads) {
-      quad(side[0].x, side[0].y, side[1].x, side[1].y, side[2].x, side[2].y, side[3].x, side[3].y);
+    for (let wall of this.allWalls) {
+      stroke(0);
+      wall.draw();
     }
+  }
+
+  drawBoundLines() {
+    stroke(hue(this.color), saturation(this.color), brightness(this.color), 100);
+    for (let bound of this.boundLines) {
+      bound.draw();
+    }
+  }
+}
+
+class Wall {
+  constructor(p0, p1, p2, p3) {
+    Object.assign(this, { p0, p1, p2, p3 });
+  }
+
+  draw() {
+    quad(this.p0.x, this.p0.y, this.p1.x, this.p1.y, this.p2.x, this.p2.y, this.p3.x, this.p3.y);
   }
 }
 
 class Line {
   constructor(p0, p1) {
-    this.p0 = p0;
-    this.p1 = p1;
+    Object.assign(this, { p0, p1 });
     this.slope = p0.x === p1.x ? 0 : (p1.y - p0.y) / (p1.x - p0.x);
     this.yIntercept = this.p0.y - this.slope * this.p0.x;
   }
