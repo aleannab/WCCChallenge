1f63c74fd798053b01a32043b3a32c85915073f6
aleannab <aleannab@gmail.com>
2023-12-18 14:13:15 -0800

    [electric-growth] Add glow

--- 2023-11-12_electric-growth/segment.js
+++ 2023-11-12_electric-growth/segment.js
@@ -10,25 +10,25 @@ class Segment {
     this.wire = new Wire(px, py, isVertical);
     this.endX = this.wire.endX;
     this.endY = this.wire.endY;
-		
-		// create flower at end point of wire
+
+    // create flower at end point of wire
     this.flower = new Flower(this.endX, this.endY);
-		
-		// init variables
+
+    // init variables
     this.isComplete = false;
-    this.alphaVal = 100;
-    this.alphaFinal = 100;
+    this.alphaVal = 255;
+    this.alphaFinal = 255;
     this.flickeringOffset = random(0, TWO_PI);
   }
 
   drawSegment() {
-		// vary alpha randomly to create flickering effect
-    this.alphaFinal = this.alphaVal + 30 * sin(millis() /10 + this.flickeringOffset);
-		
-		// animate segment growth, return true when finished
+    // vary alpha randomly to create flickering effect
+    this.alphaFinal = this.alphaVal + 30 * sin(millis() / 10 + this.flickeringOffset);
+
+    // animate segment growth, return true when finished
     if (this.wire.grow()) {
       if (this.flower.grow()) {
-        this.alphaVal -= 0.2;
+        this.alphaVal -= 1;
         return true;
       }
     }
--- 2023-11-12_electric-growth/sketch.js
+++ 2023-11-12_electric-growth/sketch.js
@@ -1,7 +1,7 @@
 // Created for the #WCCChallenge - Electric Growth
 // Electric (sketch inspired by EL wire, the line paths inspired by circuit diagrams) - Growth (flower nodes)
 
-let neonPalette = ["#da00eb", "#ff6255", "#ff009d", "#00ff53", "#01fd9a"];
+let neonPalette = ['#da00eb', '#ff6255', '#ff009d', '#00ff53', '#01fd9a'];
 let neonColorRGB;
 
 let startX, startY;
@@ -10,43 +10,42 @@ let isVertical = true;
 let allSegments = [];
 
 function setup() {
-	// init canvas/draw settings
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  // init canvas/draw settings
+  createCanvas(windowWidth, windowHeight);
   frameRate(30);
-	strokeWeight(3);
+  strokeWeight(3);
   strokeJoin(ROUND);
-  background(0);
-	
-	// pick a color
-	let neonColorStr = random(neonPalette);
-  neonColor = color(neonColorStr);
-  neonColorRGB = {r: red(neonColor), g: green(neonColor), b: blue(neonColor)};
+  noFill();
 
-	// initialize start point to canvas center
-  startX = 0;
-  startY = 0;
-	
-	// create first segment
-	createSegment();
+  // pick a color
+  let neonColorStr = random(neonPalette);
+  neonColor = color(neonColorStr);
+  neonColorRGB = { r: red(neonColor), g: green(neonColor), b: blue(neonColor) };
+
+  // initialize start point to canvas center
+  startX = width / 2;
+  startY = height / 2;
+
+  // create first segment
+  createSegment();
 }
 
 function draw() {
-  // Semi-transparent overlay
-  fill(0, 50);
-  noStroke();
-  rect(-windowWidth / 2, -windowHeight / 2, windowWidth, windowHeight);
+  background(0);
 
-	// Draw segments
-  noFill();
+  // Draw segments
+
+  drawingContext.shadowBlur = 40;
+  drawingContext.shadowColor = neonColor;
   let shouldCreateSegment = false;
   for (let i = 0; i < allSegments.length; i++) {
     let segment = allSegments[i];
     updateStrokeColor(segment.alphaFinal);
-		
-		// if the segment has finished drawing itself, flag to begin a new one
+
+    // if the segment has finished drawing itself, flag to begin a new one
     shouldCreateSegment = segment.drawSegment();
-		
-		// remove segment from array after it has finished fading out
+
+    // remove segment from array after it has finished fading out
     if (segment.alphaVal <= 0) {
       allSegments.splice(i, 1);
       i--;
@@ -55,14 +54,14 @@ function draw() {
 
   if (shouldCreateSegment) {
     createSegment();
-  }  
+  }
 }
 
 function createSegment() {
   let segment = new Segment(startX, startY, isVertical);
-	allSegments.push(segment);
-	
-	// Make next segment begins at the end of current segment
+  allSegments.push(segment);
+
+  // Make next segment begins at the end of current segment
   startX = segment.endX;
   startY = segment.endY;
 }
