d7a2a6065d55cafa44704e798cb49a0291ee03f1
aleannab <aleannab@gmail.com>
2024-02-21 15:37:12 -0800

    Reposition box element

--- 2024-02-25_iterations/sketch.js
+++ 2024-02-25_iterations/sketch.js
@@ -37,7 +37,7 @@ function draw() {
 function createNewArt() {
   gAllIterations = [];
 
-  for (let y = gBoxHeight / 2; y < height; y += gBoxHeight) {
+  for (let y = 0; y < height; y += gBoxHeight) {
     gAllIterations.push(new IterationRow(y));
   }
 }
@@ -49,13 +49,12 @@ function mouseClicked() {
 class IterationRow {
   constructor(y) {
     this.iterations = [];
-    let startVertices = [];
-    for (let i = 0; i < TWO_PI; i += PI / 30) {
-      let vX = cos(i) * gBoxWidth * 0.3;
-      let vY = sin(i) * gBoxHeight * 0.3;
-      startVertices.push(createVector(vX, vY));
-    }
-    for (let x = gBoxWidth / 2; x < width; x += gBoxWidth) {
+
+    this.min = createVector(gBoxWidth, gBoxHeight).mult(0.2);
+    this.max = createVector(gBoxWidth, gBoxHeight).mult(0.8);
+    let startVertices = this.createPath();
+
+    for (let x = 0; x < width - gBoxWidth; x += gBoxWidth) {
       let nI = new NthIteration(x, y, startVertices);
       startVertices = nI.vertices;
       this.iterations.push(nI);
@@ -64,21 +63,21 @@ class IterationRow {
 
   createPath() {
     let path = [];
-    let count = 50;
-    let inc = (0.8 * gRadius) / (count - 1);
-    path.push(createVector(gMin.x + random(0.1, 0.5) * gRadius, gMax.y));
-    path.push(createVector(gMin.x, getRandInBoundPos().y));
+    let count = 8;
+    let inc = (0.8 * gBoxWidth) / (count - 1);
+    path.push(createVector(this.min.x + random(0.1, 0.5) * gBoxWidth, gBoxHeight));
+    path.push(createVector(this.min.x, this.getRandInBoundPos().y));
     for (let i = 1; i < count; i++) {
       if (random() > 0.7) continue;
-      let xp = constrain(i * inc + 0.25 * width * random(-1, 1), gMin.x, gMax.x);
-      path.push(createVector(xp, getRandInBoundPos().y));
+      let xp = constrain(i * inc + 0.1 * gBoxWidth * random(-1, 1), this.min.x, this.max.x);
+      path.push(createVector(xp, this.getRandInBoundPos().y));
     }
-    path.push(createVector(gMax.x, gMin.y));
+    path.push(createVector(this.max.x, this.min.y));
     return path;
   }
 
   getRandInBoundPos() {
-    return createVector(random(gMin.x, gMax.x), random(gMin.y, gMax.y));
+    return createVector(random(this.min.x, this.max.x), random(this.min.y, this.max.y));
   }
 
   draw() {
@@ -116,8 +115,8 @@ class NthIteration {
   drawShape() {
     beginShape();
     for (let v of this.vertices) {
-      vertex(v.x, v.y);
+      curveVertex(v.x, v.y);
     }
-    endShape(CLOSE);
+    endShape();
   }
 }
