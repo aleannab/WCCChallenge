e457126161cdc771afe8509a7daa16292cfb3de0
aleannab <aleannab@gmail.com>
2024-02-23 19:37:12 -0800

    Fix box boundaries and experiment with shapes/lines

--- 2024-02-25_iterations/sketch.js
+++ 2024-02-25_iterations/sketch.js
@@ -2,18 +2,24 @@
 let isDebug = false;
 let gOGSettings;
 
-let gNumRows = 10;
-let gNumCols = 10;
+let gNumRows = 3;
+let gNumCols = 3;
 let gBoxWidth, gBoxHeight;
 
 let gAllIterations = [];
 
+//#, #, #, #
+// let gBgColor = '#eabaacf';
+// let gBlobPalette = ['#be3400', '#020d22', '#015045'];
 let gBgColor = '#f4f1ea';
-let gBlobPalette = ['#3567af', '#c04e82', '#538e47', '#e88740', '#016d6f', '#e25c43'];
+let gBlobPalette = ['#000000']; //'#3567af', '#c04e82', '#538e47', '#e88740', '#016d6f', '#e25c43'];
 
 function setup() {
-  let l = windowHeight < windowWidth ? windowHeight : windowWidth;
-  createCanvas(l, l);
+  let h = windowHeight < windowWidth ? windowHeight : 1.2 * windowWidth;
+  let w = windowHeight < windowWidth ? 0.8 * windowHeight : windowWidth;
+  //1080 x 1350 pixels.
+  createCanvas(w, h);
+  rectMode(CORNERS);
 
   gBoxWidth = width / gNumCols;
   gBoxHeight = height / gNumRows;
@@ -53,12 +59,12 @@ class IterationRow {
   constructor(y) {
     this.iterations = [];
 
-    this.min = createVector(gBoxWidth, gBoxHeight).mult(0.3);
-    this.max = createVector(gBoxWidth, gBoxHeight).mult(0.7);
+    this.min = createVector(gBoxWidth, gBoxHeight).mult(0.1);
+    this.max = createVector(gBoxWidth, gBoxHeight).mult(0.9);
     let startElements = [];
 
-    for (let x = 0; x < width - gBoxWidth; x += gBoxWidth) {
-      let nI = new NthIteration(x, y, startElements, this.min, this.max);
+    for (let i = 0; i < gNumCols; i++) {
+      let nI = new NthIteration(i * gBoxWidth, y, startElements, this.min, this.max);
       startElements = nI.elements.map((element) => ({ ...element }));
       this.iterations.push(nI);
     }
@@ -79,26 +85,28 @@ class NthIteration {
     this.max = max;
     this.elements = elements;
 
-    // if (random() < 0.5 || elements.length < 1) {
-    this.elements.push(this.createElement());
-    this.sortElements();
-    // }
-    this.moveRandomElement();
+    if ((elements.length < 3 && random() < 0.5) || elements.length < 1) {
+      this.elements.push(this.createElement());
+      this.sortElements();
+    } else {
+      this.moveRandomElement();
+    }
   }
 
   createElement() {
     let newElement = {
       color: random(gBlobPalette),
     };
-    if (random() < 0.5) {
+    if (true) {
+      //random() < 0.5) {
       newElement.type = 'line';
       newElement.data = this.createPath();
-      newElement.size = random(2, 5);
+      newElement.size = random(0.5, 1);
     } else {
       newElement.type = 'circle';
       newElement.data = this.getRandInBoundPos();
       newElement.size = random(this.min.x, this.max.x) * 0.5;
-      console.log(newElement.size);
+      // console.log(newElement.size);
     }
 
     return newElement;
@@ -115,23 +123,24 @@ class NthIteration {
   }
 
   moveRandomElement() {
-    for (let i = 0; i < this.elements.length; i++) {
-      let eleType = this.elements[i].type;
-      let updatedData;
-      if (eleType === 'line') {
-        updatedData = this.moveVertices(this.elements[i].data);
-      } else if (eleType === 'circle') {
-        updatedData = this.getRandInBoundPos();
-        this.elements[i].size = random(this.min.x, this.max.x) * 0.5;
-      }
-      this.elements[i].color = random(gBlobPalette);
-      this.elements[i].data = updatedData;
+    let i = floor(random(this.elements.length));
+    // for (let i = 0; i < this.elements.length; i++) {
+    let eleType = this.elements[i].type;
+    let updatedData;
+    if (eleType === 'line') {
+      updatedData = this.moveVertices(this.elements[i].data);
+    } else if (eleType === 'circle') {
+      updatedData = this.getRandInBoundPos();
+      this.elements[i].size = random(this.min.x, this.max.x) * 0.5;
     }
+    // this.elements[i].color = random(gBlobPalette);
+    this.elements[i].data = updatedData;
+    // }
   }
 
   createPath() {
     let path = [];
-    let count = 8;
+    let count = 15;
     let inc = (0.8 * gBoxWidth) / (count - 1);
     path.push(createVector(this.min.x + random(0.1, 0.5) * gBoxWidth, gBoxHeight));
     path.push(createVector(this.min.x, this.getRandInBoundPos().y));
@@ -161,6 +170,11 @@ class NthIteration {
   draw() {
     push();
     translate(this.x, this.y);
+    noStroke();
+    // fill(150);
+    // rect(0, 0, gBoxWidth, gBoxHeight);
+    fill(200);
+    rect(this.min.x, this.min.y, this.max.x, this.max.y);
     for (let e of this.elements) {
       this.drawShape(e);
     }
@@ -189,6 +203,7 @@ class NthIteration {
   drawCircle(e) {
     fill(e.color);
     noStroke();
-    ellipse(e.data.x, e.data.y, e.size);
+    rect(e.data.x, e.data.y, e.data.x + e.size, e.data.y + e.size);
+    // ellipse(e.data.x, e.data.y, e.size);
   }
 }
