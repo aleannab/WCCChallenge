90329b15f8122c68ff371fe6b8cf4f0044ca3f58
aleannab <aleannab@gmail.com>
2024-03-08 23:30:27 -0800

    Code refactor

--- 2024-03-11_firefly/sketch.js
+++ 2024-03-11_firefly/sketch.js
@@ -1,78 +1,59 @@
-// Created for the #Genuary2024 - Day 19 - Flocking
-// https://genuary.art/prompts#jan19
+// ___ by Antoinette Bumatay-Chan
+// Created for the #WCCChallenge - Topic: Firefly
+//
+//
+// See other submissions here: https://openprocessing.org/curation/78544
+// Join the Birb's Nest Discord community!  https://discord.gg/S8c7qcjw2b
 
-let gFlocks = [];
-let gFlockCount = 1;
-let gFlockSize = 300;
+let gFireflies;
+let gCount = 300;
 let gDesiredSeparation = 50;
 let gNeighborDist = 300;
 let gBuffer = 10;
 
-let gHueShift;
-
-let gPause = false;
-
-let gPalette = ['#809bce', '#95b8d1', '#b8e0d2', '#d6eadf', '#eac4d5'];
-
-let gBgColor = '#fee6e0';
+let gProps = {};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 1);
   noStroke();
-  gHueShift = -1;
+  gProps = {
+    fMax: random(0.01, 1),
+    sMax: random(4, 6),
+    sep: 0.05,
+    ali: 0.002,
+    coh: 0.001,
+  };
   makeNewFlock();
   background(0);
 }
 
 function mouseClicked() {
-  makeNewFlock();
+  //makeNewFlock();
 }
 
 function makeNewFlock() {
-  gHueShift = gHueShift < 0 ? 0 : gHueShift + random(0.2, 0.25);
-  background(gBgColor);
-
-  gFlocks = [];
-  for (let i = 0; i < gFlockCount; i++) {
-    gFlocks.push(new Flock());
-  }
-}
-
-function keyTyped() {
-  if (key === ' ') gPause = !gPause;
-  else if (key === 'c') background(gBgColor);
-  else if (key === 'n') {
-    background(gBgColor);
-    makeNewFlock();
-    gPause = false;
-  }
+  gFireflies = new Flock();
 }
 
 function draw() {
-  console.log(frameRate());
   background(0);
-  if (gPause) return;
-  for (let flock of gFlocks) {
-    flock.run();
-  }
+  gFireflies.run();
 }
 
 class Flock {
   constructor() {
-    this.col = color(random(), 1, 1, 0.05);
     this.boids = [];
 
     let props = {
-      col: random(gPalette),
       fMax: random(0.01, 1),
       sMax: random(4, 6),
-      sep: 0.05, //random(0.5, 2),
-      ali: 0.002, //random(0.5, 2),
-      coh: 0.001, //random(0.2, 0.5),
+      sep: 0.05,
+      ali: 0.002,
+      coh: 0.001,
     };
 
-    for (let i = 0; i < gFlockSize; i++) {
+    for (let i = 0; i < gCount; i++) {
       this.boids.push(new Firefly(props));
     }
   }
@@ -88,48 +69,38 @@ class Flock {
 class Firefly {
   constructor(props) {
     this.h = random(0.5, 1);
-    this.s = random(0.5, 1);
-    this.dimColor = color(this.h, this.s, 0.2, 0.1);
+    this.dimColor = color(this.h, random(0.5, 1), 0.2, 0.1);
     this.currentColor = this.dimColor;
     this.pos = createVector(random(width), random(height));
-    let angle = random(TWO_PI);
-    this.vel = createVector(cos(angle), sin(angle));
-    this.forceMax = props.fMax;
-    this.speedMax = props.sMax;
-    this.scalarS = props.sep;
-    this.scalarA = props.ali;
-    this.scalarC = props.coh;
+    this.vel = p5.Vector.random2D();
 
     this.isFlashing = false;
     this.isRecovering = false;
 
     this.readyToGoTime = 0;
     this.nextFlashTime = random(5000);
-    this.recoverInterval = 3000; //random(1000, 3000);
+    this.recoverInterval = 3000;
     this.flashInterval = 5000;
     this.r = random(5, 50);
   }
 
   run(boids) {
-    let now = millis();
-    if (this.isFlashing) {
-      if (now > this.flashDoneTime) {
-        this.isFlashing = false;
-        this.currentColor = this.dimColor;
-      }
-    } else {
-      if (this.isRecovering && now > this.readyToGoTime) {
-        this.isRecovering = false;
-      }
-
-      if (now > this.nextFlashTime) {
-        this.flash(boids, now, this.h);
-      }
-    }
-
+    this.flashCheck(boids);
     this.flock(boids);
     this.bounds();
     this.draw();
+  }
+
+  flashCheck(boids) {
+    let now = millis();
+    if (this.isFlashing && now > this.flashDoneTime) {
+      this.isFlashing = false;
+      this.currentColor = this.dimColor;
+    } else if (this.isRecovering && now > this.readyToGoTime) {
+      this.isRecovering = false;
+    } else if (!this.isFlashing && now > this.nextFlashTime) {
+      this.flash(boids, now, this.h);
+    }
   }
 
   flash(boids, now, hue) {
@@ -150,30 +121,30 @@ class Firefly {
     }
   }
 
-  flock(boids) {
-    let forceS = this.separate(boids);
-    let forceA = this.align(boids);
-    let forceC = this.cohesion(boids);
-    forceS.mult(this.scalarS);
-    forceA.mult(this.scalarA);
-    forceC.mult(this.scalarC);
+  draw() {
+    fill(this.currentColor);
+    ellipse(this.pos.x, this.pos.y, this.r);
+  }
 
-    let a = forceS;
-    a.add(forceA);
-    a.add(forceC);
+  flock(boids) {
+    let forceS = this.separate(boids).mult(gProps.sep);
+    let forceA = this.align(boids).mult(gProps.ali);
+    let forceC = this.cohesion(boids).mult(gProps.coh);
+
+    let a = forceS.add(forceA).add(forceC);
 
     this.vel.add(a);
-    this.vel.limit(this.speedMax);
+    this.vel.limit(gProps.sMax);
     this.pos.add(this.vel);
   }
 
   seek(target) {
     let desired = p5.Vector.sub(target, this.pos);
     desired.normalize();
-    desired.mult(this.speedMax);
+    desired.mult(gProps.sMax);
 
     let steer = p5.Vector.sub(desired, this.vel);
-    steer.limit(this.forceMax);
+    steer.limit(gProps.fMax);
     return steer;
   }
 
@@ -195,13 +166,14 @@ class Firefly {
     }
     if (steer.mag() > 0) {
       steer.normalize();
-      steer.mult(this.speedMax);
+      steer.mult(gProps.sMax);
       steer.sub(this.vel);
-      steer.limit(this.forceMax);
+      steer.limit(gProps.fMax);
     }
 
     return steer;
   }
+
   align(boids) {
     let sum = createVector(0, 0);
     let steer = createVector(0, 0);
@@ -217,9 +189,9 @@ class Firefly {
     if (count > 0) {
       sum.div(count);
       sum.normalize();
-      sum.mult(this.speedMax);
+      sum.mult(gProps.sMax);
       steer = p5.Vector.sub(sum, this.vel);
-      steer.limit(this.forceMax);
+      steer.limit(gProps.fMax);
     }
     return steer;
   }
@@ -241,26 +213,10 @@ class Firefly {
     return createVector(0, 0);
   }
 
-  draw() {
-    let theta = this.vel.heading() + HALF_PI;
-
-    push();
-    translate(this.pos.x, this.pos.y);
-    rotate(theta);
-    fill(this.currentColor);
-    ellipse(0, 0, this.r);
-
-    pop();
-  }
-
   bounds() {
     if (this.pos.x < -gBuffer) this.pos.x = width + gBuffer;
     if (this.pos.y < -gBuffer) this.pos.y = height + gBuffer;
     if (this.pos.x > width + gBuffer) this.pos.x = -gBuffer;
     if (this.pos.y > height + gBuffer) this.pos.y = -gBuffer;
-  }
-
-  clamp(value, minRange, maxRange) {
-    return min(max(value, minRange), maxRange);
   }
 }
