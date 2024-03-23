b2aa7f03adf3afc6558069dbba1699d6211b0d14
aleannab <aleannab@gmail.com>
2024-03-23 09:36:16 -0700

    Add quad shape

--- 2024-03-24_mesoamerican/sketch.js
+++ 2024-03-24_mesoamerican/sketch.js
@@ -16,28 +16,30 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   gUnit = width / 100;
   noLoop();
-  strokeWeight(3);
+  strokeWeight(5);
 }
 
 function draw() {
   background(255);
 
-  let unit = 0.7 * width;
-  let theta = TWO_PI / 10;
+  let count = 5;
+  let unit = (0.5 * width) / count;
+  let theta = TWO_PI / count;
   let r = 0.1 * width;
-  for (let i = 0; i < 10; i++) {
-    push();
-    translate(width / 2, height / 2);
-    let offset = random(TWO_PI);
+
+  push();
+  translate(width / 2, height / 2);
+  for (let i = 0; i < count; i++) {
+    let offset = 0; //random(TWO_PI);
     let xp = cos(theta * i + offset) * r;
     let yp = sin(theta * i + offset) * r;
     let c = new Shape(xp, yp, unit);
     c.draw();
-    pop();
 
-    unit *= random(0.8, 0.9);
-    r += random(0.5, 1) * gUnit;
+    unit *= 0.9;
+    r -= 0.1 * unit; //random(0.1, 0.6) * unit;
   }
+  pop();
 }
 
 function mouseClicked() {
@@ -52,24 +54,53 @@ class Shape {
 
     this.shapes = [];
 
-    let r = getValue('gRadius') * unit;
+    let r = unit; // getValue('gRadius') * unit;
     let n = getValue('gShapeNum', true);
     let angle = random() < 0.5 ? PI : TWO_PI;
-    for (let i = 0; i < 3; i++) {
-      this.shapes.push(this.createShape(r, n, angle));
-      r *= 0.5;
-      if (r < 10) break;
+    let isCirc = random() < 0.5;
+    if (isCirc) {
+      for (let i = 0; i < 3; i++) {
+        this.shapes.push(this.createCircle(r, n, angle));
+        r *= 0.5;
+        if (r < 10) break;
+      }
+    } else {
+      this.shapes.push(this.createQuad(r, n, angle));
     }
   }
 
-  createShape(r, n, angle) {
+  createCircle(r, n, angle) {
     let points = [];
     let angInc = angle / n;
     for (let i = 0; i < n; i++) {
       let theta = angInc * i;
       points.push(this.getPt(theta, r));
     }
-    return points;
+    return { vertices: points, lines: [] };
+  }
+
+  createQuad(r) {
+    let l = r * 2;
+    let points = [];
+    let scalars = [createVector(0, -0.1), createVector(0, 0.1), createVector(1, 0.25), createVector(1, -0.25)];
+
+    for (let i = 0; i < 4; i++) {
+      points.push(createVector(l * scalars[i].x, l * scalars[i].y));
+    }
+    console.log(points);
+
+    // Draw horizontal lines to divide the quad
+    let dividedLines = [];
+    let count = 3;
+    let inc = 1 / (count + 1);
+    for (let i = 0; i < count; i++) {
+      let xp = (i + 1) * inc * l;
+      let y0 = map(xp, 0, l, points[0].y, points[3].y);
+      let y1 = map(xp, 0, l, points[1].y, points[2].y);
+      dividedLines.push([xp, y0, xp, y1]);
+    }
+
+    return { vertices: points, lines: dividedLines };
   }
 
   getPt(theta, r) {
@@ -83,14 +114,17 @@ class Shape {
     stroke(255);
 
     push();
-    translate(this.x, this.y);
+    //translate(this.x, this.y);
     rotate(this.angle);
     for (let s of this.shapes) {
       beginShape();
-      for (let pt of s) {
+      for (let pt of s.vertices) {
         vertex(pt.x, pt.y);
       }
       endShape(CLOSE);
+      for (let pt of s.lines) {
+        line(pt[0], pt[1], pt[2], pt[3]);
+      }
     }
 
     pop();
