2e2a93ed1d36a536eef317d23a2cf006e818fb6a
aleannab <aleannab@gmail.com>
2024-03-24 00:47:34 -0700

    Add dot ring, remove unused files

--- 2024-03-24_mesoamerican/sketch.js
+++ 2024-03-24_mesoamerican/sketch.js
@@ -14,9 +14,10 @@ let gShapes;
 let gStrokeWidth = 5;
 
 function setup() {
-  let l = 0.95 * (windowWidth < windowHeight ? windowWidth : windowHeight);
-  createCanvas(l, l);
-  gUnit = width / 15;
+  let h = windowHeight < windowWidth ? windowHeight : 0.707 * windowWidth;
+  let w = windowHeight < windowWidth ? 1.414 * windowHeight : windowWidth;
+  createCanvas(0.95 * w, 0.95 * h);
+  gUnit = height / 15;
   noLoop();
   strokeWeight(gStrokeWidth);
   fill(0);
@@ -35,47 +36,50 @@ function initShapes() {
 
 class Shape {
   constructor(x, y, unit, isCirc, level, angle = -1) {
+    this.isCirc = isCirc;
+    this.level = level;
     this.pos = createVector(x, y);
     this.angle = angle != -1 ? angle : random(TWO_PI);
     this.mainShape = [];
     this.childShapes = [];
-    this.instanceCount = 1; //~~random(1, 4);
-    this.instanceInc = (level === 0 ? TWO_PI : random(QUARTER_PI, HALF_PI)) / this.instanceCount;
 
-    let n = getValue('gShapeNum', true);
-    let a = isCirc ? random(1, 3) : random(1, 5);
-    let r = a * unit;
+    // create primary shape
+    let a = isCirc ? random(1, 2) : random(2, 4);
+    this.r = a * unit;
 
     if (isCirc) {
-      let circCount = ~~random(3) + 1;
-      let cAngle = random() < 0.5 ? PI : TWO_PI;
-      for (let i = 0; i < circCount; i++) {
-        this.mainShape.push(this.createCircle(r, n, cAngle));
-        r *= random(0.3, 0.6);
-        if (r < 10) break;
-      }
+      this.createRingedCircle(this.r);
     } else {
-      this.mainShape.push(this.createQuad(r, n, angle));
+      this.mainShape.push(this.createQuad(this.r));
     }
 
-    if (unit > 6 * gStrokeWidth && level < 2) {
-      let childCount = isCirc ? 1 : 3 - level;
-      if (level === 0) childCount = 5;
+    // add child shapes
+    if (unit > 0.5 * gUnit && level < 4) {
+      let childCount = level === 0 ? 5 : 2;
       let theta = max(random(QUARTER_PI, TWO_PI) / childCount, QUARTER_PI);
 
       for (let i = 0; i < childCount; i++) {
-        let offset = 0; //random(TWO_PI);
+        let offset = random(0.25);
         let xp = a * cos(theta * i + offset) * unit;
         let yp = a * sin(theta * i + offset) * unit;
-        this.childShapes.push(new Shape(xp, yp, random(0.6, 0.9) * unit, !isCirc, level + 1, theta * i));
+        this.childShapes.push(new Shape(xp, yp, random(0.6, 0.9) * unit, getBool(), level + 1, theta * i));
       }
     }
   }
 
-  createCircle(r, n, angle) {
+  createRingedCircle(r) {
+    let circCount = ~~random(3) + 1;
+    let cAngle = getBool() ? PI : TWO_PI;
+    for (let i = 0; i < circCount; i++) {
+      this.mainShape.push(this.createCircle(r, cAngle));
+      r *= random(0.5, 0.6);
+      if (r < 4 * gStrokeWidth) break;
+    }
+  }
+  createCircle(r, angle) {
     let points = [];
-    let angInc = angle / n;
-    for (let i = 0; i < n; i++) {
+    let angInc = angle / 50;
+    for (let i = 0; i < 50; i++) {
       let theta = angInc * i;
       points.push(this.getPt(theta, r));
     }
@@ -85,38 +89,33 @@ class Shape {
   createQuad(r) {
     let points = [];
     let w0 = random(0.1, 0.4);
-    let w1 = random(0.3, 1);
-    let h = 1; //random(0.5, 1);
-    let offset = random(0.2);
-    let scalars = [createVector(0, -w0), createVector(0, w0), createVector(h, w1), createVector(h, -w1)];
+    let w1 = random(0.3, 0.5);
+    let scalars = [createVector(0, -w0), createVector(0, w0), createVector(1, w1), createVector(1, -w1)];
 
     for (let i = 0; i < 4; i++) {
       points.push(createVector(r * scalars[i].x, r * scalars[i].y));
     }
 
     let dividedLines = [];
-
-    if (r > gStrokeWidth) {
-      let count = ~~random(3) + 1;
+    if (r * w0 > gStrokeWidth) {
+      let count = ~~random(2) + 1;
       let inc = 1 / (count + 1);
-      if (inc < 8 * gStrokeWidth) {
-        let isVert = random() < 0.5;
-        for (let i = 0; i < count; i++) {
-          let xp = inc * (i + 1);
-          let x0, y0, x1, y1;
-          if (isVert) {
-            x0 = map(xp, 0, 1, points[0].x, points[3].x);
-            y0 = map(xp, 0, 1, points[0].y, points[3].y);
-            x1 = map(xp, 0, 1, points[1].x, points[2].x);
-            y1 = map(xp, 0, 1, points[1].y, points[2].y);
-          } else {
-            x0 = map(xp, 0, 1, points[0].x, points[1].x);
-            y0 = map(xp, 0, 1, points[0].y, points[1].y);
-            x1 = map(xp, 0, 1, points[3].x, points[2].x);
-            y1 = map(xp, 0, 1, points[3].y, points[2].y);
-          }
-          dividedLines.push([x0, y0, x1, y1]);
+      let isVert = getBool();
+      for (let i = 0; i < count; i++) {
+        let xp = inc * (i + 1 + random(0.5));
+        let x0, y0, x1, y1;
+        if (isVert) {
+          x0 = map(xp, 0, 1, points[0].x, points[3].x);
+          y0 = map(xp, 0, 1, points[0].y, points[3].y);
+          x1 = map(xp, 0, 1, points[1].x, points[2].x);
+          y1 = map(xp, 0, 1, points[1].y, points[2].y);
+        } else {
+          x0 = map(xp, 0, 1, points[0].x, points[1].x);
+          y0 = map(xp, 0, 1, points[0].y, points[1].y);
+          x1 = map(xp, 0, 1, points[3].x, points[2].x);
+          y1 = map(xp, 0, 1, points[3].y, points[2].y);
         }
+        dividedLines.push([x0, y0, x1, y1]);
       }
     }
 
@@ -130,22 +129,15 @@ class Shape {
   }
 
   draw() {
-    strokeWeight(random(5, 10));
+    strokeWeight(random(3, 6));
     push();
     translate(this.pos.x, this.pos.y);
     rotate(this.angle);
-    for (let i = 0; i < this.instanceCount; i++) {
-      push();
-      rotate(this.instanceInc * i);
-      // if (random() < 0.5) {
-      this.drawChildren();
-      this.drawMain();
-      // } else {
-      //   this.drawMain();
-      //   this.drawChildren();
-      // }
-      pop();
-    }
+
+    fill(0);
+    this.drawChildren();
+    this.drawMain();
+    if (this.isCirc && this.r > 0.6 * gUnit) this.drawRing();
 
     pop();
   }
@@ -168,9 +160,32 @@ class Shape {
       cs.draw();
     }
   }
+
+  drawRing() {
+    noStroke();
+    fill(255);
+
+    let count = ~~random(3, 11);
+    let theta = TWO_PI / count;
+    let r = map(count, 3, 11, 10, 3); //random(3, 10);
+    for (let i = 0; i < count; i++) {
+      let angle = theta * i;
+      let xp = 0.75 * this.r * cos(angle);
+      let yp = 0.75 * this.r * sin(angle);
+      push();
+      translate(xp, yp);
+      ellipse(0, 0, r);
+      pop();
+    }
+  }
+}
+
+function getBool() {
+  return random() < 0.5;
 }
 
 function mouseClicked() {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~');
   initShapes();
   redraw();
 }
