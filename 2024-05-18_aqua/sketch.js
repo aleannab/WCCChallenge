be006a1187819bd3f73bc28f3453defa2c64477c
aleannab <aleannab@gmail.com>
2024-05-18 21:39:09 -0700

    Add p5 brush library and adjust code to run properly locally

--- 2024-05-18_aqua/sketch.js
+++ 2024-05-18_aqua/sketch.js
@@ -1,8 +1,4 @@
-// Aquamarine by Antoinette Bumatay-Chan
-// Created for the #WCCChallenge - Topic: Aqua
-
-// I was inspired by the texture of an aquamarine crystal.
-// https://www.irocks.com/minerals/specimen/39044
+//inspo: https://www.irocks.com/minerals/specimen/39044
 
 let gPalette = ['#4F818C', '#8EBABF', '#72A0B0', '#92CAD7', '#BDD1DC'];
 let gPaletteLines = [...gPalette, '#4C5E67', '#325259'];
@@ -14,9 +10,10 @@ function setup() {
   let w = windowHeight < windowWidth ? 0.8 * windowHeight : windowWidth;
   createCanvas(0.9 * w, 0.9 * h, WEBGL);
   noLoop();
+  angleMode(DEGREES);
   background(255);
   // for whatever reason I have to set a field before being able to specify no field
-  brush.field('curved');
+  // brush.field('curved');
   brush.noField();
   brush.noStroke();
 }
@@ -27,12 +24,12 @@ function draw() {
     brush.bleed(0.03);
     brush.fillTexture(0.8, 0.5);
     brush.setHatch('pen', random(gPalette));
-    brush.hatch(random(5, 10), HALF_PI, { rand: 0, continuous: false, gradient: false });
+    brush.hatch(random(5, 10), -90, { rand: 0, continuous: false, gradient: false });
     brush.rect(width * random(-0.2, 0.2), random(-0.2, 0.2) * height, random(0.01, 0.2) * width, random(0.3, 0.6) * height, CENTER);
   }
 
   for (let i = 0; i < 100; i++) {
     brush.set(random(gBrushBox), random(gPaletteLines), random(1, 10));
-    brush.flowLine(width * randomGaussian() * 0.1, random(-0.4) * height, random(0.1, 0.5) * height, -HALF_PI);
+    brush.flowLine(width * randomGaussian() * 0.1, random(-0.4) * height, random(0.1, 0.5) * height, -90);
   }
 }
