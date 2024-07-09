f2db0f1476b91fdde8b45880de4c99156f963f32
aleannab <aleannab@gmail.com>
2024-07-08 21:37:35 -0700

    Refactor and add description

--- 2024-07-07_beneath-the-surface/sketch.js
+++ 2024-07-07_beneath-the-surface/sketch.js
@@ -1,62 +1,51 @@
-// by Antoinette Bumatay-Chan
+// Lurking by Antoinette Bumatay-Chan
 // Created for the #WCCChallenge - Topic: Beneath the Surface
+//
+// This is a work-in-progress.
+// I would still like to do the following:
+// * Avoid the faster pivoting lines in the water texture if possible
+//   (maybe by adjusting initial placement and/or movement of the voronoi control points)
+// * Play more with color palette and boat design
+//
+// Water texture is made from a voronoi diagram using c2.js.
+// I used this example as a launch off point.
+// https://renyuan.io/c2.js/examples.html?name=Voronoi
 //
 // See other submissions here: https://openprocessing.org/curation/78544
 // Join the Birb's Nest Discord community!  https://discord.gg/S8c7qcjw2b
 
-//Created by Ren Yuan
-
 let gWaterColor = '#a4e0ccf0';
+let gWaterPalette = ['#a4e0ccf0', '#a4dfccf0', '#a4e1ccf0', '#a4e0cbf0', '#a4e0cdf0'];
+
 let gWaterStroke = '#bff2d8';
 
-let gBoatColor = '#f3f3f4';
-let gBoatDarkColor = '#d5d1c3';
+let gBoatColor = '#B8B29D';
+let gBoatDarkColor = '#967653';
 let gBoatPosition;
 let gBoat;
 
 let gTentacles = [];
-let gCreatureRadius = 100;
+let gCreatureRadius;
 let gCreaturePosition;
 let gSeaCreature;
 
 let gAgents = [];
-let gAgentCount = 100;
+let gAgentCount;
 let gAgentBoatRef;
 
 let gTimeFactor;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSL, 100);
-
-  curveTightness(1);
   strokeWeight(2);
   strokeCap(SQUARE);
-
-  let maxWidth = windowWidth < windowHeight ? windowHeight : windowWidth;
 
   stroke(gWaterStroke);
   noFill();
 
   noiseDetail(4, 0.5);
 
-  for (let i = 0; i < gAgentCount; i++) {
-    gAgents.push(new Agent(random(width), random(height)));
-  }
-
-  gBoatPosition = getRulesOfThirdsPos();
-  gAgents.push(new Agent(gBoatPosition.x, gBoatPosition.y));
-  gAgentBoatRef = gAgents.length - 1;
-  gCreaturePosition = gBoatPosition.add(createVector(random(-100, 100), random(-100, 100)));
-
-  gSeaCreature = new SeaCreature(maxWidth / 2);
-  gBoat = new Boat(40, 100);
-}
-
-function getRulesOfThirdsPos() {
-  const p0 = floor(random(1, 3));
-  const p1 = floor(random(1, 3));
-  return createVector(0.33 * p0 * width, 0.33 * p1 * height);
+  initializeScene();
 }
 
 function draw() {
@@ -70,13 +59,37 @@ function draw() {
   gBoat.draw();
 }
 
+function initializeScene() {
+  let maxWidth = windowWidth < windowHeight ? windowHeight : windowWidth;
+
+  gCreatureRadius = 0.08 * maxWidth;
+  gSeaCreature = new SeaCreature(maxWidth * 0.8);
+  let bLength = 0.04 * maxWidth;
+  gBoat = new Boat(0.5 * bLength, bLength);
+  gAgentCount = floor(4 * bLength);
+
+  gAgents = [];
+  for (let i = 0; i < gAgentCount; i++) {
+    gAgents.push(new Agent(random(width), random(height)));
+  }
+
+  gBoatPosition = getRulesOfThirdsPos();
+  gAgents.push(new Agent(gBoatPosition.x, gBoatPosition.y));
+  gAgentBoatRef = gAgents.length - 1;
+  gCreaturePosition = gBoatPosition.add(createVector(random(-100, 100), random(-100, 100)));
+}
+
+function mouseClicked() {
+  initializeScene();
+}
+
 function drawWater() {
   let voronoi = new c2.Voronoi();
   voronoi.compute(gAgents);
   let regions = voronoi.regions;
 
-  curveTightness(1);
   for (let i = 0; i < regions.length; i++) {
+    fill(gAgents[i].color);
     drawPolygon(regions[i].vertices);
   }
 
@@ -88,19 +101,20 @@ function drawWater() {
 function drawPolygon(vertices) {
   stroke(gWaterStroke);
 
-  fill(gWaterColor);
+  // fill(gWaterColor);
   beginShape();
   for (let v of vertices) vertex(v.x, v.y);
   endShape(CLOSE);
 }
 
+function getRulesOfThirdsPos() {
+  const p0 = floor(random(1, 3));
+  const p1 = floor(random(1, 3));
+  return createVector(0.33 * p0 * width, 0.33 * p1 * height);
+}
+
 class SeaCreature {
   constructor(maxWidth) {
-    this.tentacles = [];
-    for (let i = 0; i < 8; i++) {
-      this.tentacles.push(new Tentacle(maxWidth, random(TWO_PI)));
-    }
-
     this.blobPts = [];
     let num = 15;
     let angleInc = TWO_PI / num;
@@ -113,6 +127,14 @@ class SeaCreature {
       let x = r * cos(angle);
       let y = r * sin(angle);
       this.blobPts.push(createVector(x, y));
+    }
+
+    this.tentacles = [];
+    let tAngle = random(TWO_PI);
+    angleInc = TWO_PI / 8;
+    for (let i = 0; i < 8; i++) {
+      this.tentacles.push(new Tentacle(maxWidth, random(TWO_PI)));
+      tAngle += random(0.2, 1) * angleInc;
     }
   }
 
@@ -143,12 +165,13 @@ class SeaCreature {
 
 class Tentacle {
   constructor(length, angle) {
-    this.control1X = length * random() * cos(angle + PI / 4);
-    this.control1Y = length * random() * sin(angle + PI / 4);
-    this.control2X = length * random() * cos(angle - PI / 4);
-    this.control2Y = length * random() * sin(angle - PI / 4);
-    this.endX = length * cos(angle);
-    this.endY = length * sin(angle);
+    this.angle = angle;
+    this.control1X = length * random() * cos(PI / 4);
+    this.control1Y = length * random() * sin(PI / 4);
+    this.control2X = length * random() * cos(PI / 4);
+    this.control2Y = length * random() * sin(PI / 4);
+    this.endX = length * cos(0);
+    this.endY = length * sin(0);
     this.sizeMax = random(0.8, 0.5);
     this.noiseSeed = random(999);
   }
@@ -157,7 +180,7 @@ class Tentacle {
     stroke(0);
     fill(0);
     push();
-    rotate(PI * noise(this.noiseSeed + 0.01 * gTimeFactor));
+    rotate(this.angle);
 
     let numPoints = 200;
     for (let t = 0; t <= 1; t += 1 / numPoints) {
@@ -170,10 +193,9 @@ class Tentacle {
 
   adjust(x) {
     randomSeed(x);
-    // Add more unpredictable wobble effect
-    let amplitude = random(200, 500); // Random amplitude
-    let frequency = random(0.1, 0.2); // Random frequency
-    let phase = random(TWO_PI); // Random phase shift
+    let amplitude = random(200, 500);
+    let frequency = random(0.1, 0.2);
+    let phase = random(TWO_PI);
 
     let wobble = amplitude * sin(frequency * x + phase + 0.1 * gTimeFactor);
     return x + wobble;
@@ -186,13 +208,14 @@ class Boat {
     this.boatHeight = boatHeight;
     this.angle = random(TWO_PI);
     this.boatPts = [
-      createVector(-boatWidth / 2, boatHeight / 2),
-      createVector(-boatWidth / 2, boatHeight / 2),
-      createVector(-boatWidth / 2, 0),
-      createVector(boatWidth / 2, 0),
-      createVector(boatWidth / 2, boatHeight / 2),
-      createVector(0, boatHeight),
-      createVector(-boatWidth / 2, boatHeight / 2),
+      createVector(-0.5 * boatWidth, 0.6 * boatHeight),
+      createVector(-0.5 * boatWidth, 0.6 * boatHeight),
+      createVector(-0.5 * boatWidth, 0),
+      createVector(0.5 * boatWidth, 0),
+      createVector(0.5 * boatWidth, 0.6 * boatHeight),
+      createVector(0.2 * boatWidth, boatHeight),
+      createVector(-0.2 * boatWidth, boatHeight),
+      createVector(-0.5 * boatWidth, 0.6 * boatHeight),
     ];
   }
 
@@ -207,23 +230,27 @@ class Boat {
     rotate(this.angle + 0.2 * noise(t));
 
     beginShape();
-    strokeWeight(5);
+    strokeWeight(2);
     this.boatPts.forEach((pt) => {
       curveVertex(pt.x, pt.y);
     });
     endShape(CLOSE);
-    strokeWeight(10);
+    strokeWeight(12);
     line(-this.boatWidth / 2, this.boatHeight / 4, this.boatWidth / 2, this.boatHeight / 4);
+
+    //person
     noStroke();
+
+    fill('#0f688b');
+    ellipse(0, 0.45 * this.boatHeight, this.boatWidth * 0.5, this.boatWidth * 0.4);
     fill('#cd364e');
-    ellipse(0, this.boatHeight / 4, 30, 10);
+    ellipse(0, 0.3 * this.boatHeight, this.boatWidth * 0.8, this.boatWidth * 0.4);
     fill(0);
-    ellipse(0, this.boatHeight / 4 + 4, 15);
+    ellipse(0, this.boatHeight / 3, this.boatWidth * 0.5);
     pop();
   }
 }
 
-// From c2 library samples
 class Agent extends c2.Point {
   constructor(xp, yp) {
     let x = xp;
@@ -231,27 +258,13 @@ class Agent extends c2.Point {
     super(x, y);
 
     this.initPosition = createVector(x, y);
+    this.color = random(gWaterPalette);
   }
 
   update() {
-    let t = gTimeFactor * 0.01;
-    this.x = this.initPosition.x + 100 * noise(0.005 * t + this.initPosition.x);
-    this.y = this.initPosition.y + 100 * noise(0.005 * t + this.initPosition.y);
-
-    if (this.x < 0) {
-      this.x = 0;
-      this.vx *= -1;
-    } else if (this.x > width) {
-      this.x = width;
-      this.vx *= -1;
-    }
-    if (this.y < 0) {
-      this.y = 0;
-      this.vy *= -1;
-    } else if (this.y > height) {
-      this.y = height;
-      this.vy *= -1;
-    }
+    let t = 0.05 * gTimeFactor;
+    this.x = this.initPosition.x + 100 * noise(t + this.initPosition.x);
+    this.y = this.initPosition.y + 100 * noise(t + this.initPosition.y);
   }
 
   display() {
