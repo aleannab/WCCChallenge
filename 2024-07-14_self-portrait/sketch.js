1700fc98b49211968aa5a747155c3a512951beac
aleannab <aleannab@gmail.com>
2024-07-13 11:57:02 -0700

    Simplify approach

--- 2024-07-14_self-portrait/sketch.js
+++ 2024-07-14_self-portrait/sketch.js
@@ -1,115 +1,90 @@
 let gImageOG;
 let gImageRef;
-let gScale;
-let gTree = [];
-let gPoints = [];
+let gDotLayers = [];
+let gDensityMin = 0.01;
+let gDensityMax = 0.1;
+let padding = 0.1;
+
+let gColorPalette = ['#d94e41', '#d9863d', '#f2b950', '#95bf93', '#46788c', '#556484'];
 
 function preload() {
   gImageOG = loadImage('test02.png');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let length = min(windowWidth, windowHeight);
+  createCanvas(length, length);
 
-  let density = 20;
-  gImageRef = gImageOG.get(0, 0, gImageOG.width, gImageOG.height);
-  gImageRef.resize(0, density);
-  gImageRef.filter(THRESHOLD);
+  gDensityMax *= length;
+  gDensityMin *= length;
+  padding *= length;
 
-  gScale = windowWidth < windowHeight ? windowWidth / gImageOG.width : windowHeight / gImageOG.height;
-  let offsetX = 0; //(windowWidth - gImageOG.width * gScale) / 2;
-  let offsetY = 0;
-  (windowHeight - gImageRef.height * gScale) / 2;
-
-  let sectionWidth = width / density;
-  let yCount = floor(height / sectionWidth);
-  let sectionHeight = height / yCount;
-  gImageRef.loadPixels();
-  for (let y = 0; y < gImageRef.height; y++) {
-    for (let x = 0; x < gImageRef.width; x++) {
-      let index = (x + y * gImageRef.width) * 4;
-      if (gImageRef.pixels[index] === 0) {
-        gPoints.push(createVector(sectionWidth * x + offsetX, sectionHeight * y + offsetY));
-      }
-    }
-  }
-  console.log('gPoints count: ' + gPoints.length);
-
-  gTree = createNodes(0, 0, width, height, density);
+  setupDotLayers();
   noLoop();
 }
 
 function draw() {
   background(255);
+  translate(padding, padding);
 
   fill(0);
-  gPoints.forEach((pt) => {
-    ellipse(pt.x, pt.y, 50);
+  gDotLayers.forEach((layer) => {
+    layer.draw();
   });
-
-  fill(255);
-
-  gTree.draw();
 }
 
-function createNodes(x, y, w, h, density) {
-  let currentQuadrant = new Quadrant(x, y);
-  if (density > 20) return currentQuadrant;
+function mouseClicked() {
+  setupDotLayers();
+}
 
-  // get relevant part of image and resize accordingly
-  let image = gImageOG.get(x, y, gImageOG.width, gImageOG.height);
-  image.resize(0, density);
-  image.filter(THRESHOLD);
+function setupDotLayers() {
+  gColorPalette = shuffle(gColorPalette);
 
-  image.loadPixels();
-  let sectionWidth = width / density;
-  let yCount = floor(height / sectionWidth);
-  let sectionHeight = height / yCount;
+  gDotLayers = [];
+  let densities = [];
+  for (let i = 0; i < gColorPalette.length; i++) {
+    densities.push(int(random(gDensityMin, gDensityMax)));
+  }
+  console.log(densities);
 
-  currentQuadrant.points = [];
-  for (let y = 0; y < image.height; y++) {
-    for (let x = 0; x < image.width; x++) {
-      let index = (x + y * image.width) * 4;
-      if (image.pixels[index] === 0) {
-        currentQuadrant.points.push(createVector(sectionWidth * x, sectionHeight * y));
-      }
-    }
+  densities = sort(densities);
+  for (let i = 0; i < densities.length; i++) {
+    gDotLayers.push(new DotLayer(densities[i], gColorPalette[i % gColorPalette.length]));
   }
 
-  console.log('tree count: ' + currentQuadrant.points.length);
-
-  let halfW = floor(w / 2);
-  let halfH = floor(h / 2);
-  // console.log('half size: ' + halfW + ' ' + halfH);
-  let newDensity = 2 * density;
-  currentQuadrant.quadrants.push(createNodes(x, y, halfW, halfH, newDensity)); // Top-left
-  currentQuadrant.quadrants.push(createNodes(x + halfW, y, halfW, halfH, newDensity)); // Top-right
-  currentQuadrant.quadrants.push(createNodes(x, y + halfH, halfW, halfH, newDensity)); // Bottom-left
-  currentQuadrant.quadrants.push(createNodes(x + halfW, y + halfH, halfW, halfH, newDensity)); // Bottom-right
-
-  return currentQuadrant;
+  redraw();
 }
 
-class Quadrant {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+class DotLayer {
+  constructor(density, dotColor) {
+    let image = gImageOG.get(0, 0, gImageOG.width, gImageOG.height);
+    image.resize(0, density);
+    image.filter(THRESHOLD);
 
+    let sectionWidth = (width - 2 * padding) / density;
+    let yCount = floor((height - 2 * padding) / sectionWidth);
+    let sectionHeight = (height - 2 * padding) / yCount;
+    image.loadPixels();
     this.points = [];
-    this.quadrants = [];
+    for (let y = 0; y < image.height; y++) {
+      for (let x = 0; x < image.width; x++) {
+        let index = (x + y * image.width) * 4;
+        if (image.pixels[index] === 0) {
+          this.points.push(createVector(sectionWidth * x, sectionHeight * y));
+        }
+      }
+    }
+    this.size = map(density, gDensityMax, gDensityMin, 0.3, 0.9) * sectionWidth;
+
+    this.color = dotColor;
   }
 
   draw() {
-    fill(255, 0, 0);
-    push();
-    translate(this.x, this.y);
+    fill(this.color);
+    stroke(this.color);
+
     this.points.forEach((pt) => {
-      ellipse(pt.x, pt.y, 5);
+      ellipse(pt.x, pt.y, this.size);
     });
-    this.quadrants.forEach((q) => {
-      // q.draw();
-    });
-    // this.quadrants[0].draw();
-    pop();
   }
 }
