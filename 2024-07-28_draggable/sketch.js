16e7dc3c028ca4479ceb399bf7978e367031a6bc
aleannab <aleannab@gmail.com>
2024-08-07 21:54:37 -0700

    Final draggable changes

--- 2024-07-28_draggable/sketch.js
+++ 2024-07-28_draggable/sketch.js
@@ -1,81 +1,95 @@
-// Tangram by Antoinette Bumatay-Chan
+// Fragmented Square Puzzle by Antoinette Bumatay-Chan
 // Created for the #WCCChallenge - Topic: Draggable
+//
+// Click and drag the pieces around to complete the square.
+// Once completed, click again for new puzzle
 //
 // See other submissions here: https://openprocessing.org/curation/78544
 // Join the Birb's Nest Discord community!  https://discord.gg/S8c7qcjw2b
 
-let gShapes = [];
+let gTriangles = [];
 let gSelected = null;
 let gOffset = null;
 let gSideLength;
+let gAreaMin;
+let gDifficulty = 2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSL, 100);
-  strokeWeight(5);
-  strokeJoin(BEVEL);
 
+  colorMode(HSL, 100);
+  strokeWeight(2);
+  strokeJoin(BEVEL);
   rectMode(CENTER);
 
   gSideLength = min(windowWidth, windowHeight) * 0.6;
+
+  initPuzzle();
+}
+
+function draw() {
+  background(95);
+  noStroke();
+  fill(80);
+  rect(width / 2, height / 2, gSideLength, gSideLength);
+  gTriangles.forEach((triangle) => {
+    triangle.draw();
+  });
+}
+
+function initPuzzle() {
+  let totalArea = gSideLength * gSideLength;
+  gAreaMin = totalArea / ++gDifficulty;
+  gTriangles = [];
+
   let xOffset = (windowWidth - gSideLength) / 2;
   let yOffset = (windowHeight - gSideLength) / 2;
 
+  // centered square points
   let p1 = createVector(xOffset, yOffset);
   let p2 = createVector(xOffset + gSideLength, yOffset);
   let p3 = createVector(xOffset, yOffset + gSideLength);
   let p4 = createVector(xOffset + gSideLength, yOffset + gSideLength);
 
+  // split square into two triangles, and subdivide
   subdivide({ p1: p1, p2: p2, p3: p3 });
   subdivide({ p1: p2, p2: p3, p3: p4 });
 
-  let hInc = 100 / gShapes.length;
+  // set color and move to random position
+  let hInc = 100 / gTriangles.length;
   let h = int(random(100));
-  for (let i = 0; i < gShapes.length; i++) {
-    gShapes[i].color = color(h, 100, 60);
-    gShapes[i].move();
+  for (let i = 0; i < gTriangles.length; i++) {
+    gTriangles[i].color = color(h, 80, 60);
+    gTriangles[i].move();
     h = (h + hInc) % 100;
   }
-}
-
-function draw() {
-  background(100);
-  noStroke();
-  fill(80);
-  rect(width / 2, height / 2, gSideLength, gSideLength);
-  for (let i = 0; i < gShapes.length; i++) {
-    gShapes[i].draw();
-  }
-}
-
-function randomColor() {
-  return color(random(0, 8), random(30, 60), random(20, 100));
 }
 
 function triangleArea(p1, p2, p3) {
   return abs((p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) / 2);
 }
 
-function subdivide(shape) {
-  let minArea = 200 * gSideLength;
-  if (triangleArea(shape.p1, shape.p2, shape.p3) < minArea) {
-    gShapes.push(new Shape(shape.p1, shape.p2, shape.p3));
+function subdivide(triangle) {
+  // add triangle if size is small enough
+  if (triangleArea(triangle.p1, triangle.p2, triangle.p3) < gAreaMin) {
+    gTriangles.push(new Triangle(triangle.p1, triangle.p2, triangle.p3));
     return;
   }
 
-  let a = p5.Vector.lerp(shape.p1, shape.p2, random(0.1, 0.9));
-  let b = p5.Vector.lerp(shape.p2, shape.p3, random(0.1, 0.9));
-  let c = p5.Vector.lerp(shape.p3, shape.p1, random(0.1, 0.9));
-  subdivide({ p1: shape.p1, p2: a, p3: c });
-  subdivide({ p1: shape.p2, p2: b, p3: a });
-  subdivide({ p1: shape.p3, p2: c, p3: b });
+  // otherwise triangle divide further
+  let a = p5.Vector.lerp(triangle.p1, triangle.p2, random(0.1, 0.9));
+  let b = p5.Vector.lerp(triangle.p2, triangle.p3, random(0.1, 0.9));
+  let c = p5.Vector.lerp(triangle.p3, triangle.p1, random(0.1, 0.9));
+  subdivide({ p1: triangle.p1, p2: a, p3: c });
+  subdivide({ p1: triangle.p2, p2: b, p3: a });
+  subdivide({ p1: triangle.p3, p2: c, p3: b });
   subdivide({ p1: a, p2: b, p3: c });
 }
 
-function containsPoint(shape, px, py) {
-  let b1 = sign(px, py, shape.p1, shape.p2) < 0.0;
-  let b2 = sign(px, py, shape.p2, shape.p3) < 0.0;
-  let b3 = sign(px, py, shape.p3, shape.p1) < 0.0;
+function containsPoint(triangle, px, py) {
+  let b1 = sign(px, py, triangle.p1, triangle.p2) < 0.0;
+  let b2 = sign(px, py, triangle.p2, triangle.p3) < 0.0;
+  let b3 = sign(px, py, triangle.p3, triangle.p1) < 0.0;
   return b1 === b2 && b2 === b3;
 }
 
@@ -84,11 +98,23 @@ function sign(px, py, p1, p2) {
 }
 
 function mousePressed() {
-  for (let i = gShapes.length - 1; i >= 0; i--) {
-    if (gShapes[i].isStatic) continue;
-    if (containsPoint(gShapes[i], mouseX, mouseY)) {
-      gSelected = gShapes.splice(i, 1)[0];
-      gShapes.push(gSelected);
+  let isPuzzleComplete = true;
+  for (let i = 0; i < gTriangles.length; i++) {
+    if (!gTriangles[i].isStatic) {
+      isPuzzleComplete = false;
+      break;
+    }
+  }
+
+  if (isPuzzleComplete) {
+    initPuzzle();
+  }
+
+  for (let i = gTriangles.length - 1; i >= 0; i--) {
+    if (gTriangles[i].isStatic) continue;
+    if (containsPoint(gTriangles[i], mouseX, mouseY)) {
+      gSelected = gTriangles.splice(i, 1)[0];
+      gTriangles.push(gSelected);
       gOffset = createVector(mouseX, mouseY).sub(createVector(gSelected.cx, gSelected.cy));
       break;
     }
@@ -105,14 +131,14 @@ function mouseDragged() {
 }
 
 function mouseReleased() {
-  if (gSelected.isStatic) {
-    let lastItem = gShapes.pop();
-    gShapes.unshift(lastItem);
+  if (gSelected && gSelected.isStatic) {
+    let lastItem = gTriangles.pop();
+    gTriangles.unshift(lastItem);
   }
   gSelected = null;
 }
 
-class Shape {
+class Triangle {
   constructor(p1, p2, p3) {
     this.ogP1 = p1;
     this.ogP2 = p2;
@@ -120,9 +146,9 @@ class Shape {
 
     let centroid = createVector((p1.x + p2.x + p3.x) / 3, (p1.y + p2.y + p3.y) / 3);
 
-    // Calculate max offsets to keep shape within the canvas bounds
-    let maxOffsetX = min(centroid.x, width - centroid.x);
-    let maxOffsetY = min(centroid.y, height - centroid.y);
+    // Calculate max offsets to keep triangle within the canvas bounds
+    let maxOffsetX = min(centroid.x, 0.8 * width - centroid.x);
+    let maxOffsetY = min(centroid.y, 0.8 * height - centroid.y);
 
     // Add random offset within bounds
     let offset = createVector(random(-maxOffsetX, maxOffsetX), random(-maxOffsetY, maxOffsetY));
@@ -134,7 +160,6 @@ class Shape {
     this.cx = (this.p1.x + this.p2.x + this.p3.x) / 3;
     this.cy = (this.p1.y + this.p2.y + this.p3.y) / 3;
 
-    this.color = randomColor();
     this.isStatic = false;
   }
 
